142ff8029181a1ba1d2759bf70982c6474229efd
LuciosProjects <oellaufer92@gmail.com>
2025-12-24 15:56:37 +0200

    Refactor launch configuration and update test scripts for improved clarity and functionality

--- tests/test_PR.py
+++ tests/test_PR.py
@@ -1,4 +1,6 @@
 # -------- Standard library imports --------
+import datetime
+import functools
 
 # -------- Third-party imports --------
 import pytest
@@ -10,6 +12,11 @@ from pysft.lib import fetchData
 from testIndicators import indicatorsDB
 
 MAX_INDICATOR_STR_PER_LINE = 10
+TEST_MODULE_SPACER = "-"
+TEST_MODULE_HALF_SEPARATOR = TEST_MODULE_SPACER * 50
+TEST_MODULE_SEPARATOR = TEST_MODULE_HALF_SEPARATOR * 2
+
+# Utilities
 
 def print_indicators_list(indicators: list[str]) -> None:
     """
@@ -27,8 +34,63 @@ def print_indicators_list(indicators: list[str]) -> None:
         indicatorStr += f"{indicator}, "
         counter += 1
 
-    print(f"Indicators:\n{indicatorStr}")
+    print(f"Indicators:\n{indicatorStr}\n")
 
+def wrap_string_in_banner(str_in: str) -> str:
+    """
+    Utility function to wrap a string in a banner for better visibility in logs.
+    """
+
+    # Top of banner
+    bannered_str = f"\n{TEST_MODULE_SEPARATOR}\n"
+
+    # Middle segment
+    len_mid_segment =  (len(TEST_MODULE_SEPARATOR) - len(str_in)) / 2
+
+    if len_mid_segment % 1 == 0:
+        bannered_str += f"{TEST_MODULE_SPACER * int(len_mid_segment)}{str_in}{TEST_MODULE_SPACER * int(len_mid_segment)}\n"
+    else:
+        bannered_str += f"{TEST_MODULE_SPACER * int(len_mid_segment)}{str_in}{TEST_MODULE_SPACER * (int(len_mid_segment) + 1)}\n"
+
+    # Bottom of banner
+    bannered_str += f"{TEST_MODULE_SEPARATOR}\n"
+    return bannered_str
+
+# PR tests banner decorator
+def banner(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        # Configure middle banner segments
+        mid_segment_start = f" Test START ({fn.__name__}) " 
+        mid_segment_end = f" Test END ({fn.__name__}) "
+
+        mid_segment_start_full = wrap_string_in_banner(mid_segment_start)
+        mid_segment_end_full = wrap_string_in_banner(mid_segment_end)
+
+        print("\n"*2)
+        print(mid_segment_start_full)
+        try:
+            fn(*args, **kwargs)
+        finally:
+            print("\n")
+            print(mid_segment_end_full)
+
+    return wrapper
+
+@pytest.fixture(scope="module", autouse=True)
+def _module_banner():
+    module_banner_start_full = wrap_string_in_banner(" Starting test_PR.py tests ")
+    module_banner_end_full = wrap_string_in_banner(" Finished test_PR.py tests ")
+
+    print(module_banner_start_full)
+    print(f"Test start time: {datetime.datetime.now()}\n")
+    print(TEST_MODULE_SEPARATOR + "\n")
+    yield
+    print(module_banner_end_full)
+    print(f"Test end time: {datetime.datetime.now()}\n")
+    print(TEST_MODULE_SEPARATOR + "\n")
+
+# pytest fixures
 @pytest.fixture
 def getTestIndicators() -> list[str]:
     indicators = indicatorsDB.US
@@ -45,7 +107,10 @@ def getRequiredAttributes() -> list[str]:
 def getDateRange() -> tuple[str, str]:
     return ("2025-07-01", "2025-08-01")
 
+# Test functions
+
 # Single indicator fetch test
+@banner
 def test_single_indicator_fetch(getTestIndicators):
     """
     Test fetching data for a single indicator.
@@ -76,6 +141,7 @@ def test_single_indicator_fetch(getTestIndicators):
     print(f"Single indicator fetch test for {singleInd} passed.")
 
 # Multiple indicators fetch test, with date range
+@banner
 def test_multiple_indicators_fetch(getTestIndicators, getDateRange):
     """
     Test fetching data for multiple indicators.
@@ -112,6 +178,7 @@ def test_multiple_indicators_fetch(getTestIndicators, getDateRange):
     print(f"Multiple indicators fetch test for total: {len(getTestIndicators)} indicators passed.")
 
 # Multiple indicators fetch test, with required attributes and date range
+@banner
 def test_fetch_with_required_attributes(getTestIndicators, getRequiredAttributes, getDateRange):
     """
     Test fetching data for multiple indicators with specific required attributes.
