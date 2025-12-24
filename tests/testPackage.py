d826f5dd9490ecc391c266b429e107c2fada2ccf
LuciosProjects <oellaufer92@gmail.com>
2025-12-24 15:56:37 +0200

    Refactor launch configuration and update test scripts for improved clarity and functionality

--- tests/testPackage.py
+++ tests/testPackage.py
@@ -3,21 +3,20 @@ import pysft
 from testIndicators import indicatorsDB
 
 if __name__ == "__main__":
-    # indicators = indicatorsDB.PORTFOLIO
-    indicators = ["AAPL", "MSFT", "GOOG", "TSLA", "NVDA"] 
+    indicators = indicatorsDB.TASE
 
     # quote = pysft.lib.fetchData(indicators, start="2025-07-01", end="2025-08-01")
 
     # Simple single indicator fetch (should only give current price)
-    quote = pysft.lib.fetchData("AAPL")
+    # quote = pysft.lib.fetchData(indicators[0])
 
     # Simple multiple indicators fetch (should only give current prices)
-    quote = pysft.lib.fetchData(indicators)
+    # quote = pysft.lib.fetchData(indicators)
 
     # Historical data fetch
-    quote = pysft.lib.fetchData(indicators, start="2024-01-01", end="2024-06-30", interval="1d")
+    # quote = pysft.lib.fetchData(indicators, start="2024-01-01", end="2024-06-30", interval="1d")
 
     # All together fetch
     quote = pysft.lib.fetchData(indicators, attributes=["name", "price", "high", "low", "open", "volume"], period="1m")
 
-    quote = pysft.lib.fetchData(indicators, attributes=["name", "price", "high", "low", "open", "volume"], start="2024-05-01", end="2024-06-30", interval="1d")
+    # quote = pysft.lib.fetchData(indicators, attributes=["name", "price", "high", "low", "open", "volume"], start="2025-07-01", end="2025-08-01")
